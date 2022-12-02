764356b2fbebce580ae1781da9a14b6dbeb652a0
Ernestinebot <116901955+Ernestinebot@users.noreply.github.com>
2022-12-02 11:21:58 -0500

    Initial commit

---
+++ src/squarepattern/SquarePattern.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package squarepattern;
+
+/**
+ *
+ * @author ernestine. I am practicing using sout tab and System.out.println 
+ */
+public class SquarePattern {   //must be saved as SquarePattern.java
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("* * * * *");
+         System.out.println("*       *");
+         System.out.println("*       *");
+         System.out.println("*       *");
+         System.out.println("* * * * *");
+    }
+    
+}
